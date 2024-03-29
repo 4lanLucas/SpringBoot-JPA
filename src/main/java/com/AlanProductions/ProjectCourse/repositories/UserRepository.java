20deb6e7a9a57f8e2dff8ba1d3797cc7f49b9ce0
4lan Lucas <alanlucas0707@gmail.com>
2024-03-29 15:19:53 -0300

    added Service layer, and findAll and findById

--- src/main/java/com/AlanProductions/ProjectCourse/repositories/UserRepository.java
+++ src/main/java/com/AlanProductions/ProjectCourse/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.AlanProductions.ProjectCourse.repositories;
 import com.AlanProductions.ProjectCourse.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/AlanProductions/ProjectCourse/services/UserService.java
@@ -0,0 +1,24 @@
+package com.AlanProductions.ProjectCourse.services;
+
+import com.AlanProductions.ProjectCourse.entities.User;
+import com.AlanProductions.ProjectCourse.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> optional = userRepository.findById(id);
+        return optional.get();
+    }
+}
