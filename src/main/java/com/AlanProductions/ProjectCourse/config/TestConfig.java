9d648e743f38ef5d92fbd67fd8b3306e4be07b46
4lan Lucas <alanlucas0707@gmail.com>
2024-03-30 10:48:18 -0300

    added OrderStatus enum

--- src/main/java/com/AlanProductions/ProjectCourse/config/TestConfig.java
+++ src/main/java/com/AlanProductions/ProjectCourse/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.AlanProductions.ProjectCourse.config;
 
 import com.AlanProductions.ProjectCourse.entities.Order;
 import com.AlanProductions.ProjectCourse.entities.User;
+import com.AlanProductions.ProjectCourse.entities.enums.OrderStatus;
 import com.AlanProductions.ProjectCourse.repositories.OrderRepository;
 import com.AlanProductions.ProjectCourse.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null,"Maria Brown", "maria@gmail.com", "9888888", "123456");
         User u2 = new User(null,"Alex Green", "Alex@gmail.com", "9777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),u1, OrderStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),u2,OrderStatus.WAITING_PAYMENT);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),u1,OrderStatus.DELIVERED);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
